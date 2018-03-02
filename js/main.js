0d61db019a38a8b89b74d1139a9fa9777818a23d
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-03-02 12:38:09 -0500

    WIP

--- js/main.js
+++ js/main.js
@@ -6,26 +6,106 @@ const game = (() => {
                 0,0,0,
                 0,0,0
               ];
+  let isTwoPlayers = false;
+  let difficulty = 0;
+  let players = [];
 
   const setNumberOfPlayers = (id) => {
-    if (id === "1-player") {
-      return 1
-    } else {
-      return 2;
+    // if (id === "1-player") {
+    //   return false;
+    // } else {
+      isTwoPlayers = true;
+    //   return true;
+    // }
+  };
+
+  const setDifficulty = (id) => {
+    // 0 = normal, 1 = hard
+    if (id === 'hard') {
+      difficulty = 1;
     }
   };
 
   const reset = () => {
     // TODO
+    isTwoPlayers = false;
+    difficulty = 0;
+    // TODO toggle visibility
   };
 
-  const start = (difficulty) => {
-    // TODO
+  const init = (inputs) => {
+    let names = inputs.filter((input) => input.value !== '');
+    console.log(names);
+    // names.forEach((name, index) => {
+    //   // if (input.value !== '') {
+    //     game.createPlayer(input.value, index);
+    //   // }
+    // });
+
+    if (players.length === 1) {
+      // create computer player
+      players.push(computerPlayer());
+      toggle.visibility("difficulty-select"); // show
+
+    } else {
+      start();
+    }
+
   };
 
-  return { board, setNumberOfPlayers, reset, start };
+  const start = () => {
+    toggle.visibility("info");
+    toggle.visibility("board");
+  };
+
+  const createPlayer = (name, index) => {
+    let p = player(name, index);
+    players.push(p);
+  }
+
+  const debug = () => {
+    console.log('isTwoPlayers: ' + isTwoPlayers + '\n' +
+                'difficulty: ' + difficulty + '\n');
+                players.forEach((player) => {
+                  console.log(player.name);
+                  console.log(player.playerMark);
+                });
+  }
+
+  return { board, setNumberOfPlayers, reset, setDifficulty, start, init, debug, createPlayer };
 })();
 
+// ==================== Player ================================================
+const player = (name, index) => {
+  let marks = ['X', 'O'];
+  let score = 0;
+  let playerMark = marks[index];
+
+  return { name, score, playerMark };
+};
+
+// ==================== Computer Player =======================================
+const computerPlayer = () => {
+  // TODO difficulty level
+  // TODO logic for each difficulty level
+  const name = "CPU";
+  const playerMark = 'O';
+
+  return { name, playerMark };
+}
+
+// ==================== AI ====================================================
+const ai = () => {
+  const randomizer = (multiplier) => {
+    return Math.floor(Math.random() * multiplier);
+  };
+
+  const difficulty = () => {
+    // TODO
+  };
+};
+
+// ==================== HANDLERS =============================================
 const handlers = (() => {
   const clickHandler = (e) => {
     const elementClicked = e.target;
@@ -38,18 +118,22 @@ const handlers = (() => {
         toggle.visibility("name-input");
         break;
       case "name-input-submit-btn" :
-        toggle.visibility("difficulty-select");
-        toggle.visibility("name-input");
+        let inputs = elements.inputs;
+        console.log(inputs);
+        game.init(inputs);
         break;
       case "normal" :
       case "hard" :
-        // TODO start
+        game.setDifficulty();
+        game.start();
         break;
     }
 
     if (elementClicked.classList.contains("tile")) {
       // TODO
       console.log(`tile #${elementClicked.id}`);
+    } else if (elementClicked.classList.contains("debug")) {
+      game.debug();
     }
   };
   const display = {};
@@ -70,6 +154,14 @@ const toggle = (() => {
 
   return { visibility };
 })();
+
+const elements = (() => {
+  // const btns = document.querySelectorAll("button");
+  // const tiles = document.querySelectorAll('.tile');
+  const inputs = document.querySelectorAll("input");
+  return { inputs };
+})();
+
 window.addEventListener("load", function() {
   document.addEventListener("click", handlers.clickHandler, false );
 });
