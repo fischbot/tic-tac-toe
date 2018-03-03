4285e93b9e093a124f5a3078ff3ccb0bdfde9675
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-03-03 16:25:03 -0500

    add display.nameInputs to create inputs for setting player names

--- js/main.js
+++ js/main.js
@@ -30,6 +30,23 @@ const ai = () => {
   };
 };
 
+// ==================== Display ===============================================
+
+const display = (() => {
+  const nameInputs = (numOfPlayers) => {
+    // create a label and input element for 1 or 2 players
+    for (let i = 0; i < numOfPlayers; i++) {
+      let input = elements.create("input", ".name");
+      let label = elements.create("label", "");
+      label.innerText = "Player " + (i + 1) + " Name: ";
+      elements.append("#name-input", label);
+      elements.append("#name-input", input);
+    }
+  };
+
+  return { nameInputs };
+})();
+
 const toggle = (() => {
   const visibility = (id) => {
     const element = document.getElementById(`${id}`);
