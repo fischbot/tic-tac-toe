a51bfd0ee483ba19e000895934a96aff9b2e0d21
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-03-03 19:38:27 -0500

    game.checkForWin
    iterates over board to see if either X or O won

--- js/game.js
+++ js/game.js
@@ -12,8 +12,54 @@ const game = (() => {
     numOfPlayers : 1
   };
 
-  const checkForWin = () => {
-    // TODO
+  const checkForWin = () => { // TODO refactor
+    let boardStartAndEndPoints = [
+      [0,2],[3,5],[6,8],[0,6,3],[1,7,3],[2,8,3],[0,8,4],[2,6,2]
+    ];
+    let isRoundOver = false;
+    let total = 0;
+    while (!isRoundOver) {
+      boardStartAndEndPoints.forEach((set) => {
+        if (set.length === 2) {
+          // check horizontal
+          for (let i = set[0], end = set[1]; i <= end; i++) {
+            if (board[i] === 'X') {
+              total += 1;
+            } else if (board[i] === 'O') {
+              total += 100;
+            }
+          }
+        } else {
+          // check vertical and diagonal
+          let addTiles = set[2];
+          for (let i = set[0], end = set[1]; i <= end; i += addTiles) {
+            if (board[i] === 'X') {
+              total += 1;
+            }
+
+            if (board[i] === 'O') {
+              total += 100;
+            }
+          }
+        }
+
+        // Check for win
+        if (total === 3) {
+          // gameOver("X");
+          alert("X Wins!");
+          isWon = true;
+          isRoundOver = true;
+        } else if (total === 300) {
+          // gameOver("O");
+          alert("O Wins!");
+          isWon = true;
+          isRoundOver = true;
+        } else {
+          total = 0;
+        }
+      }); // end forEach
+      isRoundOver = true;
+    } // end while
   };
 
   const createPlayer = (name, index) => {
