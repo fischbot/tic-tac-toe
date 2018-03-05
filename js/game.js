d696909d30b5a34ede1eb096ec0448a565d64d74
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-03-05 14:10:39 -0500

    refactor checkForWin

--- js/game.js
+++ js/game.js
@@ -13,44 +13,30 @@ const game = (() => {
     numOfPlayers : 1
   };
 
-  const checkForWin = () => { // TODO refactor
+  const checkForWin = () => {
     let boardStartAndEndPoints = [
       // index 0 = start location, index 1 = end location,
       // if there is an index 2 = how many tiles to add
-      [0,2],[3,5],[6,8],[0,6,3],[1,7,3],[2,8,3],[0,8,4],[2,6,2]
+      [0,2,1],[3,5,1],[6,8,1],[0,6,3],[1,7,3],[2,8,3],[0,8,4],[2,6,2]
     ];
     let isRoundOver = false;
     let total = 0;
     while (!isRoundOver) {
       boardStartAndEndPoints.forEach((set) => {
-        if (set.length === 2) {
-          // check horizontal
-          for (let i = set[0], end = set[1]; i <= end; i++) {
-            if (board[i] === 'X') {
-              total += 1;
-            } else if (board[i] === 'O') {
-              total += 100;
-            }
-          }
-        } else {
-          // check vertical and diagonal
-          let addTiles = set[2];
-          for (let i = set[0], end = set[1]; i <= end; i += addTiles) {
-            if (board[i] === 'X') {
-              total += 1;
-            }
-
-            if (board[i] === 'O') {
-              total += 100;
-            }
+        let addTiles = set[2];
+        for (let i = set[0], end = set[1]; i <= end; i+= addTiles) {
+          if (board[i] === 'X') {
+            total += 1;
+          } else if (board[i] === 'O') {
+            total += 100;
           }
         }
 
         // Check for win
         if (total === 3 || total === 300) {
-          gameOver(players[turn]);
           isRoundOver = true;
           isWon = true;
+          gameOver(`${players[turn].name} wins!`);
         } else {
           total = 0;
         }
