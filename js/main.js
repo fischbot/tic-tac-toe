8059ebb28eeb63d0c3251c962e0746b957ce60fb
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-03-08 21:57:35 -0500

    more player to player.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 "use strict";
 
-// ==================== Player ================================================
-const player = (name, index) => {
-  let marks = ['X', 'O'];
-  let score = 0;
-  let playerMark = marks[index];
-  return { name, score, playerMark };
-};
 
 // ==================== Display ===============================================
 
--- js/player.js
+++ js/player.js
@@ -1,1 +1,7 @@
 "use strict";
+const player = (name, index) => {
+  let marks = ['X', 'O'];
+  let score = 0;
+  let playerMark = marks[index];
+  return { name, score, playerMark };
+};
